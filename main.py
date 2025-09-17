00e2231ab10906c886190e51ceb91bf0a76f974d
domyself1 <2470065375@qq.com>
2025-09-17 16:20:24 +0800

    完成本次作业

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-# 回文数判断程序
-n = input("请输入一个5位数字：")
-# 判断输入是否为纯数字，且长度是否为5
-if not n.isdigit() or len(n) != 5:
-    print("输入错误：必须输入一个5位数字！")
-else:
-    # 判断是否为回文数
-    if n == n[::-1]:  
-        print("是回文数")
+def main():
+    num = input().strip()  # 读取输入并去除首尾空格
+    # 判断输入是否为纯数字，且长度为5
+    if not num.isdigit() or len(num) != 5:
+        print("输入错误: 请输入5位数字")
     else:
-        print("不是回文数")
+        if num == num[::-1]:
+            print("是回文数")
+        else:
+            print("不是回文数")
+if __name__ == "__main__":
+    main()
